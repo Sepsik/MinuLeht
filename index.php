60f19ac8a64d07ded62f3444e6e128205ec7141d
Anita Sepp <anisepp@klass2-17-l.intra.itcollege.ee>
2017-02-11 16:28:55 +0200

    lihtne html, css ja veidi php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<title> Anita leht </title>
+	</head>
+	<body>
+		<h1> Hei! </h1>
+		<img src="pics/foto.png" alt="yup, that should be me here"/>
+		<p id="theQuote"> <?php echo "Everything is going to be ok in the end, if it's not ok it is not the end."; ?> </p>
+	</body>
+</html>
